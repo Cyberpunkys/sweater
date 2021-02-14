760dd5f71959aba302c196a3fef40979b7442dda
Cyberpunkys <h4k3r88@gmail.com>
2021-02-14 23:49:19 +0300

    add Flyway migration add user profile editing

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -8,7 +8,7 @@ public class Message {
 
     @Id //Поле будет индентификатором
     @GeneratedValue(strategy= GenerationType.AUTO) //auto generate
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
@@ -32,11 +32,11 @@ public class Message {
         return author != null ? author.getUsername() : "<none>";
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
